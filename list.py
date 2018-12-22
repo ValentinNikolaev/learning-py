71116983a0819a38ec98f3ff8eac148a01b8770b
ValentinNikolaev <valenti.nikolaev@gmail.com>
2018-12-22 12:01:58 +0200

    add list lesson

---
+++ list.py
@@ -0,0 +1,12 @@
+numbers = [1,3,45,5,5,5,6,6,4.5]
+strings = ['str','ste34']
+names = ['Ruslan', 'Valentin', 'Serg']
+second_name = ['Tereshkova', 'Gagarin','Nikolaev']
+#append
+numbers.append(44)
+strings.append('appended string')
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name[1])
